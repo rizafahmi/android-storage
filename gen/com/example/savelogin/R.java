3a4579f75863241c8d9f008d977d6f95ac609ebd
Riza Fahmi <rizafahmi@gmail.com>
2016-05-29 19:21:47 +0700

    Finish save preference

--- gen/com/example/savelogin/R.java
+++ gen/com/example/savelogin/R.java
@@ -13,6 +13,15 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int dataTextView=0x7f060006;
+        public static final int loadButton=0x7f060005;
+        public static final int passwordEditText=0x7f060003;
+        public static final int saveButton=0x7f060004;
+        public static final int textView1=0x7f060000;
+        public static final int textView2=0x7f060001;
+        public static final int usernameEditText=0x7f060002;
+    }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
--- src/com/example/savelogin/MainActivity.java
+++ src/com/example/savelogin/MainActivity.java
@@ -1,15 +1,43 @@
 package com.example.savelogin;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private EditText usernameEditText;
+	private EditText passwordEditText;
+	private TextView dataTextView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+		passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+		dataTextView = (TextView) findViewById(R.id.dataTextView);
+ 	}
+	
+	public void savePref(View view) {
+		SharedPreferences pref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+		
+		SharedPreferences.Editor editor = pref.edit();
+		editor.putString("username", usernameEditText.getText().toString());
+		editor.putString("password", passwordEditText.getText().toString());
+		
+		editor.apply();
+		
+		Toast.makeText(this, "Saved!", Toast.LENGTH_LONG).show();
 	}
+	
+	
 }
