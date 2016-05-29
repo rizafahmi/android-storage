77ab1e0d5a00a27ac8359b873be7698bf15cc9f5
Riza Fahmi <rizafahmi@gmail.com>
2016-05-29 22:02:35 +0700

    Save logins to internal storage

--- src/com/example/savelogin/MainActivity.java
+++ src/com/example/savelogin/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.savelogin;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -16,6 +20,8 @@ public class MainActivity extends Activity {
 	private EditText usernameEditText;
 	private EditText passwordEditText;
 	private TextView dataTextView;
+	private FileOutputStream fos;
+	private String FILENAME = "UserInfo";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,18 @@ public class MainActivity extends Activity {
 
 		usernameEditText.setText(pref.getString("username", ""));
 		passwordEditText.setText(pref.getString("password", ""));
+		
+//		
+//		try {
+//			fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+//			fos.close();
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
  	}
 	
 	public void savePref(View view) {
@@ -40,6 +58,22 @@ public class MainActivity extends Activity {
 		editor.putString("password", passwordEditText.getText().toString());
 		
 		editor.apply();
+		
+		String data = usernameEditText.getText().toString() + ":" + passwordEditText.getText().toString();		
+		// Save to internal storage
+		try {
+			fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+			fos.write(data.getBytes());
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
 		
 		Toast.makeText(this, "Saved!", Toast.LENGTH_LONG).show();
 	}
